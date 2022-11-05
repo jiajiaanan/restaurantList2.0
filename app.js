5116b6d1b22fbb0cae852e455c3f3982a8e0476e
jiajiaanan <jiajiaanan@gmail.com>
2022-11-05 09:57:32 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 require('./config/mongoose')
+
 
 const routes = require('./routes')
 const app = express()
@@ -11,15 +13,20 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// 載入session驗證功能
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 // setting static files
 app.use(express.static('public'))
-//設定 body-parser
+// 設定 body-parser
 app.use(express.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
 // 載入路由模組
 app.use(routes)
-
 
 
 app.listen(port, () => {
